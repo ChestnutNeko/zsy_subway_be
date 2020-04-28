9c05f9bec84cce7498751732312b2f8afac16f55
zhanshiyu <zhanshiyu@jd.com>
2020-04-28 19:41:19 +0800

    update

--- express.sql
+++ express.sql
@@ -13,9 +13,9 @@ MySQL - 5.5.40 : Database - test
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`zsy_subway_bd` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`zsy_subway_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `zsy_subway_bd`;
+USE `zsy_subway_db`;
 
 /*Table structure for table `personal_routes` */
 
