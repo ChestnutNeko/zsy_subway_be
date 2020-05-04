e8edef0a0665685d336dec3df9a0522aefa647ef
ChestnutNeko <1014044412@qq.com>
2020-05-04 10:31:38 +0800

    update

--- express.sql
+++ express.sql
@@ -13,10 +13,10 @@ DROP TABLE IF EXISTS `personal_routes`;
 CREATE TABLE `personal_routes` (
   `routes_id` int(20) NOT NULL AUTO_INCREMENT COMMENT '收藏路线编号',
   `user_id` int(20) NOT NULL COMMENT '用户编号',
-  `user_name` varchar(255) DEFAULT NULL COMMENT '用户名',
-  `routes_name` varchar(255) DEFAULT NULL COMMENT '路线名',
-  `routes_start` varchar(255) DEFAULT NULL COMMENT '起始点',
-  `routes_end` varchar(255) DEFAULT NULL COMMENT '终点',
+  `user_name` varchar(255) NOT NULL COMMENT '用户名',
+  `routes_name` varchar(255) NOT NULL COMMENT '路线名',
+  `routes_start` varchar(255) NOT NULL COMMENT '起始点',
+  `routes_end` varchar(255) NOT NULL COMMENT '终点',
   `routes_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `routes_collect` int(20) DEFAULT NULL COMMENT '是否可以取消收藏',
   PRIMARY KEY (`routes_id`)
@@ -32,9 +32,9 @@ DROP TABLE IF EXISTS `user_info`;
 
 CREATE TABLE `user_info` (
   `user_id` int(20) NOT NULL AUTO_INCREMENT COMMENT '用户编号',
-  `user_name` char(255) DEFAULT NULL COMMENT '用户名',
-  `user_password` char(255) DEFAULT NULL COMMENT '密码',
-  `user_type` int(20) NULL COMMENT '用户类型',
+  `user_name` char(255) NOT NULL COMMENT '用户名',
+  `user_password` char(255) NOT NULL COMMENT '密码',
+  `user_type` int(20) NOT NULL COMMENT '用户类型',
   `user_city` varchar(255) NULL COMMENT '城市',
   `user_telephone` varchar(255) NULL COMMENT '电话',
   `user_subway` varchar(20) NULL COMMENT '地铁',
@@ -51,9 +51,9 @@ insert into `user_info`(`user_id`,`user_name`,`user_password`,`user_type`,`user_
 DROP TABLE IF EXISTS `personal_goods`;
 
 CREATE TABLE `personal_goods` (
-  `the_lost_id` int(20) NOT NULL AUTO_INCREMENT COMMENT '收藏失物编号',
+  `id` int(20) NOT NULL AUTO_INCREMENT COMMENT '收藏失物编号',
+  `the_lost_id` int(20) NOT NULL COMMENT '失物编号',
   `user_id` int(20) NOT NULL COMMENT '用户编号',
-  `user_name` char(255) DEFAULT NULL COMMENT '用户名',
   `the_lost_name` varchar(255) DEFAULT NULL COMMENT '失物名称',
   `the_lost_city` varchar(255) DEFAULT NULL COMMENT '城市',
   `the_lost_value` int(20) DEFAULT NULL COMMENT '预估金额（元）',
@@ -66,7 +66,7 @@ CREATE TABLE `personal_goods` (
 
 /*Data for the table `personal_goods` */
 
-insert into `personal_goods`(`the_lost_id`,`user_id`,`user_name`,`the_lost_name`,`the_lost_city`,`the_lost_value`,`the_lost_date`,`the_lost_position`,`the_lost_telephone`,`the_lost_collect`) values (20,12,'liz','iphone6s','北京',2000,'2020-03-05 15:06:28','经海路站','13900001111',0),(21,24,'zsy','书包','哈尔滨',888,'2020-03-04 11:06:28','黑龙江大学站','13522345001',1);
+insert into `personal_goods`(`id`,`the_lost_id`,`user_id`,`the_lost_name`,`the_lost_city`,`the_lost_value`,`the_lost_date`,`the_lost_position`,`the_lost_telephone`,`the_lost_collect`) values (111,20,12,'iphone6s','北京',2000,'2020-03-05 15:06:28','经海路站','13900001111',0),(121,21,24,'书包','哈尔滨',888,'2020-03-04 11:06:28','黑龙江大学站','13522345001',1);
 
 /*Table structure for table `the_lost_list` */
 
@@ -74,7 +74,7 @@ DROP TABLE IF EXISTS `the_lost_list`;
 
 CREATE TABLE `the_lost_list` (
   `the_lost_id` int(20) NOT NULL AUTO_INCREMENT COMMENT '失物编号',
-  `the_lost_name` varchar(255) DEFAULT NULL COMMENT '失物名称',
+  `the_lost_name` varchar(255) NOT NULL COMMENT '失物名称',
   `the_lost_city` varchar(255) DEFAULT NULL COMMENT '城市',
   `the_lost_value` int(20) DEFAULT NULL COMMENT '预估金额（元）',
   `the_lost_date` timestamp NULL DEFAULT current_timestamp COMMENT '日期',
